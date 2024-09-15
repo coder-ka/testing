b32e28d3cd3de0d361ed4d0501068beae7439024
Katsuyuki Oeda <katsu.amagramer@gmail.com>
2024-09-15 17:51:12 +0900

    ph1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
-export function add(a: number, b: number) {
-  return a + b;
+export type DoResult = {
+  children: TestResult[];
+};
+export type TestResult = DoResult & {
+  skipped: boolean;
+  description?: string;
+};
+export type Test = {
+  skipped: boolean;
+  do(test: (param: {}) => Promise<DoResult | void>): Promise<TestResult>;
+  skip(): Test;
+};
+export function test(description?: string): Test {
+  return {
+    skipped: false,
+    async do(test) {
+      if (this.skipped)
+        return {
+          skipped: true,
+          children: [],
+        };
+
+      const result = await test({});
+
+      return {
+        ...(result || { children: [] }),
+        skipped: this.skipped,
+        description,
+      };
+    },
+    skip() {
+      this.skipped = true;
+      return this;
+    },
+  };
 }
--- tests/index.ts
+++
@@ -1,4 +0,0 @@
-import assert from "assert";
-import { add } from "../src";
-
-assert.deepStrictEqual(add(1, 2), 3);
